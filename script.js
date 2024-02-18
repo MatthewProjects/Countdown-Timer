471139ea6d4a170db81e5b24c4145ca43cb3fd8a
Matt Troughton <mvt0408@gmail.com>
2024-02-17 20:03:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const currentDate = new Date();
 console.log(currentDate); // This logs the current date and time to the console
+
+const targetDate = new Date('YYYY-MM-DDTHH:MM:SS'); // Replace with target
+console.log(targetDate); // Check the target date in the console
